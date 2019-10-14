7d193031b54422c69deb0f85a3643772ccec18bd
AurelienZintzmeyer <firansac@gmail.com>
2019-10-14 19:18:52 +0200

    Add disconnect log & a ping every second (socket)

--- server/socket.js
+++ server/socket.js
@@ -9,8 +9,18 @@ module.exports.init = function(app){
     console.log("socket io listening on", io.path());
 
     io.on('connection', (socket) => { 
-        console.log("new socket ---> " + socket.handshake.address);
-        socket.emit("a", "hello")
+        console.log(`[${new Date().toLocaleTimeString()}] new socket --->  ${socket.handshake.address}`);
+        let timeout = setInterval(() => {
+            if(socket.connected){
+                socket.emit("a", {a: "hello"}); 
+            } else {
+                clearInterval(timeout);
+            }
+        }, 1000);
+
+        socket.on("disconnect" ,()=> {
+            console.log(`[${new Date().toLocaleTimeString()}] socket on ${socket.handshake.address} has disconnected`);
+        });
     });
 }
 
