f7b8b728924aa6f59a3d6d222b4f0aa27f73bae9
AurelienZintzmeyer <firansac@gmail.com>
2020-01-30 19:28:52 +0100

    remote-server: add mqtt

--- remote-server/index.js
+++ remote-server/index.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {publish} = require('./mqtt');
 const PORT = 8000;
+
+
+
 
 const app = express()
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
 app.use(express.static('public'))
+
 app.post('/login', (req, res) => {
     res.redirect('/home.html')
+})
+
+app.post('/toggle', async (req, res) => {
+    const r = publish('toggle');
+    if(typeof r == 'string'){
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(500).json(r.message)
+    }
 })
 
 
---
+++ remote-server/mqtt.js
@@ -0,0 +1,40 @@
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://localhost:1883')
+
+const REMOTE_ACTION_TOPIC = 'remote_action';
+
+
+client.on('connect', function () {
+
+    client.subscribe(REMOTE_ACTION_TOPIC, function (err) {
+        if (!err) {
+        client.publish(REMOTE_ACTION_TOPIC, 'Hello mqtt')
+        }
+    })
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(topic, ':', message.toString())
+})
+
+/**
+ * @param {string} str 
+ * @returns {Error|string}
+ */
+async function publish(str){
+   const res = await new Promise((resolve, reject) => {
+
+    client.once('error', (error) => {
+      reject(error);
+    })
+
+    client.publish(REMOTE_ACTION_TOPIC, str);
+    setTimeout(() => resolve('ok'), 10)
+  });
+
+  return res;
+
+}
+
+module.exports = {publish};
